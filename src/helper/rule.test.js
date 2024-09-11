585089571e24ac0cf3443ab61f5194288cd7fa30
Shun_Tseng <shun_tseng@migocorp.com>
2024-09-11 16:58:24 +0800

    feat: 新增 checkColumnsName 相關測試

--- src/helper/rule.test.js
+++ src/helper/rule.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import { checkColumnsCount } from "./rule";
+import { checkColumnsCount, checkColumnsName } from "./rule";
 
 describe("checkColumnsCount", () => {
   test("欄位數量符合需求", () => {
@@ -26,6 +26,36 @@ describe("checkColumnsCount", () => {
     const expectedResult = {
       isPassed: false,
       text: "檔案欄位數量與規格不符，請確認有無遺漏/多餘欄位"
+    }
+
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+describe("checkColumnsName", () => {
+  test("欄位名稱與要求相符", () => {
+    const jsonData = [
+      ["Name", "Age"]
+    ];
+    const expectedNames = ["Name", "Age"];
+    const result = checkColumnsName({ jsonData, expectedNames });
+    const expectedResult = {
+      isPassed: true,
+      text: ""
+    }
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("欄位名稱與要求相符", () => {
+    const jsonData = [
+      ["Name", "Age"]
+    ];
+    const expectedNames = ["Name", "Phone"];
+    const result = checkColumnsName({ jsonData, expectedNames });
+    const expectedResult = {
+      isPassed: false,
+      text: "Age 不在規格範圍內, 請調整欄位名稱"
     }
 
     expect(result).toEqual(expectedResult);
